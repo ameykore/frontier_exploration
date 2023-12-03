15b80a6b344d3be046d7b9f746aa4bb98b05868e
ameykore <ameyakore@gmail.com>
2023-12-03 22:48:21 +0000

    added tutorial pakage

---
+++ build/tutorial_pkg/CMakeFiles/drive_controller_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/frontier_expo/frontier_exploration/devel/.private/tutorial_pkg/lib/tutorial_pkg/drive_controller_node"
+  "/frontier_expo/frontier_exploration/devel/.private/tutorial_pkg/lib/tutorial_pkg/drive_controller_node.pdb"
+  "CMakeFiles/drive_controller_node.dir/src/drive_controller.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/drive_controller_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/tutorial_pkg/CMakeFiles/tutorial_pkg_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/frontier_expo/frontier_exploration/devel/.private/tutorial_pkg/lib/tutorial_pkg/tutorial_pkg_node"
+  "/frontier_expo/frontier_exploration/devel/.private/tutorial_pkg/lib/tutorial_pkg/tutorial_pkg_node.pdb"
+  "CMakeFiles/tutorial_pkg_node.dir/src/tutorial_pkg_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tutorial_pkg_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
