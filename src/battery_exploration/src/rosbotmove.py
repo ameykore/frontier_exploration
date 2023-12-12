d63a4863bae5dc3c4c7a24a4b7780f1f60be5c36
ameykore <ameyakore@gmail.com>
2023-12-12 22:52:52 +0000

    rosrobt wtih paused postion code

--- src/battery_exploration/src/rosbotmove.py
+++ src/battery_exploration/src/rosbotmove.py
@@ -1,48 +1,49 @@
 #!/usr/bin/env python3
-# license removed for brevity
 
 import rospy
-
-# Brings in the SimpleActionClient
 import actionlib
-# Brings in the .action file and messages used by the move base action
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseActionFeedback
+
+# constant
+position = 0
+
+def feedback_callback(feedback):
+    global position
+    position = feedback.feedback.base_position.pose
+    print(type(position))
 
 def movebase_client():
 
    # Create an action client called "move_base" with action definition file "MoveBaseAction"
     client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
- 
    # Waits until the action server has started up and started listening for goals.
     client.wait_for_server()
 
    # Creates a new goal with the MoveBaseGoal constructor
     goal = MoveBaseGoal()
     goal.target_pose.header.frame_id = "map"
-    goal.target_pose.header.stamp = rospy.Time.now()
-   # Move 0.5 meters forward along the x axis of the "map" coordinate frame 
-    goal.target_pose.pose.position.x = 0.5
-   # No rotation of the mobile base frame w.r.t. map frame
-    goal.target_pose.pose.orientation.w = 1.0
+    goal.target_pose.pose.position.x = position.position.x
+    goal.target_pose.pose.position.y = position.position.y
+    goal.target_pose.pose.position.z = position.position.z
+    goal.target_pose.pose.orientation.w = position.orientation.w
+    goal.target_pose.pose.orientation.x = position.orientation.x
+    goal.target_pose.pose.orientation.y = position.orientation.y
+    goal.target_pose.pose.orientation.z = position.orientation.z
 
-   # Sends the goal to the action server.
     client.send_goal(goal)
-   # Waits for the server to finish performing the action.
-    wait = client.wait_for_result()
-   # If the result doesn't arrive, assume the Server is not available
-    if not wait:
-        rospy.logerr("Action server not available!")
-        rospy.signal_shutdown("Action server not available!")
-    else:
-    # Result of executing the action
-        return client.get_result()   
+    client.wait_for_result() 
 
-# If the python node is executed as main process (sourced directly)
 if __name__ == '__main__':
     try:
-       # Initializes a rospy node to let the SimpleActionClient publish and subscribe
         rospy.init_node('movebase_client_py')
-        result = movebase_client()
+
+        rospy.Subscriber("move_base/feedback", MoveBaseActionFeedback, feedback_callback)
+
+        rate = rospy.Rate(10) # 10hz
+        while not rospy.is_shutdown():
+            result = movebase_client()
+            rate.sleep() 
+            print("execution hault")
         if result:
             rospy.loginfo("Goal execution done!")
     except rospy.ROSInterruptException:
