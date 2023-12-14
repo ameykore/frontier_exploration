74d256e1ea7c5ec0976c7f22000d8e0a06ebe8fb
ameykore <ameyakore@gmail.com>
2023-12-14 15:43:19 +0000

    rosbot working with battery drain

--- src/battery_exploration/src/rosbotmove.py
+++ src/battery_exploration/src/rosbotmove.py
@@ -2,49 +2,52 @@
 
 import rospy
 import actionlib
+import std_msgs.msg as msg
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseActionFeedback
 
-# constant
+# Constants
 position = 0
+voltage = 10
+
+def battery_feedback(battery):
+    global voltage
+    print(f"Received battery voltage: {battery.data}")
+    voltage = battery.data
+    rate = rospy.Rate(10)  # 10hz
+    while voltage <= 1.0:
+        print("Low battery")
+        movebase_client()
+        rate.sleep()
 
 def feedback_callback(feedback):
     global position
     position = feedback.feedback.base_position.pose
-    print(type(position))
 
 def movebase_client():
+    global position
 
-   # Create an action client called "move_base" with action definition file "MoveBaseAction"
-    client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
-   # Waits until the action server has started up and started listening for goals.
+    client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
     client.wait_for_server()
 
-   # Creates a new goal with the MoveBaseGoal constructor
     goal = MoveBaseGoal()
     goal.target_pose.header.frame_id = "map"
-    goal.target_pose.pose.position.x = position.position.x
-    goal.target_pose.pose.position.y = position.position.y
-    goal.target_pose.pose.position.z = position.position.z
-    goal.target_pose.pose.orientation.w = position.orientation.w
-    goal.target_pose.pose.orientation.x = position.orientation.x
-    goal.target_pose.pose.orientation.y = position.orientation.y
-    goal.target_pose.pose.orientation.z = position.orientation.z
+    goal.target_pose.pose = position
 
     client.send_goal(goal)
-    client.wait_for_result() 
+    client.wait_for_result()
+
+def receiver():
+    global voltage
+
+    rospy.init_node('battery_receiver', anonymous=True)
+    rospy.Subscriber("Battery/voltage", msg.Float32, battery_feedback)
+    rospy.Subscriber("move_base/feedback", MoveBaseActionFeedback, feedback_callback)
+
+    # Continue listening until the node is shut down
+    rospy.spin()
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('movebase_client_py')
-
-        rospy.Subscriber("move_base/feedback", MoveBaseActionFeedback, feedback_callback)
-
-        rate = rospy.Rate(10) # 10hz
-        while not rospy.is_shutdown():
-            result = movebase_client()
-            rate.sleep() 
-            print("execution hault")
-        if result:
-            rospy.loginfo("Goal execution done!")
+        receiver()
     except rospy.ROSInterruptException:
         rospy.loginfo("Navigation test finished.")
